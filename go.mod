2e727ec8d068f4e88ac2fad096a94fc00df89dcf
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2021-12-19 20:58:30 +0700

    feat:add paramter to method Hello_world

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/YogaAPSY/go-hello-world
+module github.com/YogaAPSY/go-hello-world/v2
 
 go 1.17
--- hello_world.go
+++ hello_world.go
@@ -1,9 +1,6 @@
 package hello_world
 
-import "fmt"
+func Hello_world(name string) string {
 
-func Hello_world() string {
-	fmt.Println("Hello Cruel World")
-
-	return "Hello World"
+	return "Welcome to Cruel World " + name
 }
