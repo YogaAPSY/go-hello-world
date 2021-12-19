a44d61f83a302102e1b411630fc362d4a80183ca
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2021-12-19 20:38:04 +0700

    hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YogaAPSY/go-hello-world
+
+go 1.17
---
+++ hello_world.go
@@ -0,0 +1,9 @@
+package hello_world
+
+import "fmt"
+
+func Hello_world() string {
+	fmt.Println("Hello World")
+
+	return "Hello World"
+}
